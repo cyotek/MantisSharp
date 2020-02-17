8e7f5358de3367cd6d1ae8087f176c41e8f55475
Richard Moss <richard.moss@cyotek.com>
2020-02-17 17:35:10 +0000

    Updated for 0.0.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,22 +1,36 @@
 Change Log
 ==========
 
+## 0.4.0
+
+### Fixed
+
+* A crash could occur due to incorrect processing of the
+  response character set
+
 ## 0.3.0
+
 ### Added
-* Added `MantisClient.CreateIssue` with various overloads for creating new issues
+
+* Added `MantisClient.CreateIssue` with various overloads for
+  creating new issues
 * Added `MantisClient.DeleteIssue(int)`
-* Added `MantisClient.GetConfigValues` with various overloads for returning configuration settings
+* Added `MantisClient.GetConfigValues` with various overloads
+  for returning configuration settings
 * Browser sample can now retrieve configuration settings
 * Browser sample can now delete issues
 * Browser sample can now create issues
 
 ### Changed
+
 * All properties are now settable
 
 ## 0.0.2
 
 ### Added
+
 * Added support for issue relationships
 
 ## 0.0.1
+
 * Initial release
--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -14,14 +14,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Cyotek Ltd")]
 [assembly: AssemblyProduct("MantisSharp")]
-[assembly: AssemblyCopyright("Copyright © 2017 Cyotek Ltd. All Rights Reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2017-2020 Cyotek Ltd. All Rights Reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("005D3685-E2B1-4E70-86CC-363F0268B27B")]
 [assembly: CLSCompliant(true)]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.2")]
-[assembly: AssemblyInformationalVersion("0.0.2-alpha")]
+[assembly: AssemblyFileVersion("0.0.0.4")]
+[assembly: AssemblyInformationalVersion("0.0.4-alpha")]
 
 [assembly: InternalsVisibleTo("MantisSharp.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100C913BEF347DB625E04945A3F62F4E14380B7B879F87529624A5299D1657AEA310A56CC53F3FBF0013A27748C0FEBBD1DFC730849E11BA2792C426FC15749CF8A18A6E1511D52FE0380B9BB947E7CFB3AF658E2FF09769A0325988E12E730C9DF537A0E8CD35E27FD52381B9538BF5A57E534A93611D787A8B592EE5F35ADBF93")]
